68134aae8d2e89ca38ad9ef10a02680ae3970dc6
Snoopy20salvia <salvianoorc20@gmail.com>
2025-05-14 14:56:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("via in pixels 🚢")
-st.write( "just a small space to share some thoughts and ideas."
+st.write("just a small space to share some thoughts and ideas."
 )
-
+st.image("view/young.png", width=200)
+st.write("\n")
