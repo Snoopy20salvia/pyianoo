8e38318c316daaa46baaeb3833f21ace6147d83d
Snoopy20salvia <salvianoorc20@gmail.com>
2025-05-14 15:00:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("via in pixels 🚢")
+st.title("via in pixels 🦦")
 st.write("just a small space to share some thoughts and ideas."
 )
 st.image("https://github.com/Snoopy20salvia/pyianoo/blob/main/51682c20b649d9604f842cf837b88926.jpg", width=200)
