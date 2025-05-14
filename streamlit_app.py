1b48028566006df972e470715f17c161290d419c
Snoopy20salvia <salvianoorc20@gmail.com>
2025-05-14 14:45:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("via in pixels 🚢")
+st.write( "just a small space to share some thoughts and ideas."
 )
+
