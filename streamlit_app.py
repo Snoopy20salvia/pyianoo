7d732ce3282178454e3f36c28d60340be9b1d590
Snoopy20salvia <salvianoorc20@gmail.com>
2025-05-21 14:59:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,11 @@ st.write("just a small space to share some thoughts and ideas."
 )
 st.image("51682c20b649d9604f842cf837b88926.jpg", width=200)
 st.write("\n")
+st.title("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
