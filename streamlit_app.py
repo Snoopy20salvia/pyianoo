c4759400f260a32cfe534e2a74749d2740f46970
Snoopy20salvia <salvianoorc20@gmail.com>
2025-05-21 14:59:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ st.write("just a small space to share some thoughts and ideas."
 )
 st.image("51682c20b649d9604f842cf837b88926.jpg", width=200)
 st.write("\n")
+st.write("a cute little puppy")
 st.title("Aplikasi Sederhana")
 st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
 angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
